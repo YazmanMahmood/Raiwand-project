6c228c83ca46559dbc22674d969367cf1519a9e9
YazmanMahmood <156944466+YazmanMahmood@users.noreply.github.com>
2024-06-24 18:28:58 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,21 @@
- 
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, onValue, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAmVSOm8g6p4F3ZY4jxIEUTQH_oFllo1hg",
-    authDomain: "greenhouse-raiwind.firebaseapp.com",
-    databaseURL: "https://greenhouse-raiwind-default-rtdb.firebaseio.com",
-    projectId: "greenhouse-raiwind",
-    storageBucket: "greenhouse-raiwind.appspot.com",
-    messagingSenderId: "338760023791",
-    appId: "1:338760023791:web:667a022e8b69459eb2651a",
-    measurementId: "G-CPD0XFLYN5"
-  };
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "greenhouse-raiwind.firebaseapp.com",
+  databaseURL: "https://greenhouse-raiwind-default-rtdb.firebaseio.com",
+  projectId: "greenhouse-raiwind",
+  storageBucket: "greenhouse-raiwind.appspot.com",
+  messagingSenderId: "338760023791",
+  appId: "1:338760023791:web:667a022e8b69459eb2651a",
+  measurementId: "G-CPD0XFLYN5"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app); // Get the database instance
 
-export { database, ref, onValue, update };
-
-
+export { database, ref, onValue, update }; // Export Firebase functions for use in other modules
