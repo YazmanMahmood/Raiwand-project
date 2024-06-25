be8359af72ac3cfa06e15b04e71c6f5ec56c0c70
YazmanMahmood <156944466+YazmanMahmood@users.noreply.github.com>
2024-06-25 14:16:00 +0500

    Update bay1.js

--- bay1.js
+++ bay1.js
@@ -16,7 +16,7 @@ function toggleWater() {
 
   // Update water state in Firebase
   const waterState = isWaterOn ? 1 : 0; // Change here to send 1 or 0
-  update(ref(database, 'GreenHouse Raiwind/ESP1/ESP_20240622030452'), {
+  update(ref(database, '/GreenHouse Raiwind/ESP1/ESP_20240622030456/water flow'), {
     waterState: waterState
   });
 
