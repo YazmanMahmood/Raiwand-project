74f0bc3118e21778337691d29cdeb099fbbb398f
YazmanMahmood <156944466+YazmanMahmood@users.noreply.github.com>
2024-07-14 15:25:07 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,26 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
+    const loginForm = document.getElementById('loginForm');
+    const errorMessage = document.getElementById('error-message');
+
+    loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-        const errorMessage = document.getElementById('error-message');
 
-        if (username === 'admin' && password === 'yazman') {
-            // Save login state
-            sessionStorage.setItem('loggedIn', 'true');
-            // Redirect to main page (index.html)
-            window.location.href = 'index.html';
-        } else {
-            errorMessage.textContent = 'Invalid username or password';
-        }
+        // Sign in with Firebase Authentication
+        firebase.auth().signInWithEmailAndPassword(username, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                sessionStorage.setItem('loggedIn', 'true');
+                // Redirect to bays.html
+                window.location.href = 'bays.html';
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                errorMessage.textContent = errorMessage;
+            });
     });
 });
