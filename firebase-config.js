6bcb13f1315574344c217ca9ab1577eba1db9b2d
YazmanMahmood <156944466+YazmanMahmood@users.noreply.github.com>
2024-07-27 22:14:45 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,9 @@
+// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
-// Your Firebase configuration
-const firebaseConfig = {
+import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
+
+export const firebaseConfig = {
   apiKey: "AIzaSyAmVSOm8g6p4F3ZY4jxIEUTQH_oFllo1hg",
   authDomain: "greenhouse-raiwind.firebaseapp.com",
   databaseURL: "https://greenhouse-raiwind-default-rtdb.firebaseio.com",
@@ -11,7 +13,10 @@ const firebaseConfig = {
   appId: "1:338760023791:web:667a022e8b69459eb2651a",
   measurementId: "G-CPD0XFLYN5"
 };
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+
 console.log("Firebase initialized");
-export { database, ref, onValue };
+
+export { app, database, ref, set, onValue, get, child };
