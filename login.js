44ecc42a55b11ae27030775b5ba6cf5c3d858f78
YazmanMahmood <156944466+YazmanMahmood@users.noreply.github.com>
2024-08-31 15:29:20 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,54 +1,41 @@
 // login.js
 import { database, ref, get } from './firebase-config.js';
+import { startSessionTimer } from './session-manager.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded');
     const loginForm = document.getElementById('loginForm');
     const errorMessage = document.getElementById('error-message');
 
-    if (!loginForm) {
-        console.error('Login form not found');
-        return;
-    }
-
     loginForm.addEventListener('submit', function(event) {
-        console.log('Form submitted');
         event.preventDefault();
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-
-        console.log('Attempting login for username:', username);
 
         // Reference to the users in the database
         const usersRef = ref(database, 'users');
 
         // Check if the entered username exists in the database
         get(usersRef).then((snapshot) => {
-            console.log('Database snapshot received');
             if (snapshot.exists()) {
                 const users = snapshot.val();
-                console.log('Users data:', users);
                 if (users[username]) {
                     const storedPassword = users[username].password;
                     if (password === storedPassword) {
-                        console.log('Login successful');
                         // Credentials are correct
                         sessionStorage.setItem('loggedIn', 'true');
+                        startSessionTimer(); // Start the session timer
                         window.location.href = 'bays.html'; // Redirect to bays.html on successful login
                     } else {
-                        console.log('Invalid password');
                         showError('Invalid username or password');
                     }
                 } else {
-                    console.log('Username not found');
                     showError('Invalid username or password');
                 }
             } else {
-                console.log('No users found in database');
                 showError('An error occurred. Please try again.');
             }
         }).catch((error) => {
-            console.error('Database error:', error);
+            console.error('Error:', error);
             showError('An error occurred. Please try again.');
         });
     });
